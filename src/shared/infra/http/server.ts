9f5dd402f0250eda8addfc75730f1462085881bb
Augusto Kestering Longo <augusto.kestering@gmail.com>
2021-01-26 21:33:32 -0300

    typerom config, folders structures

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+import routes from './routes';
+
+import '@shared/infra/typeorm';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('💥 Server Start!');
+});
